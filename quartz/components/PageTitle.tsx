2ea00926b742101081c98f3c288881624f04133b
fredxgit <frederik.rix@gmail.com>
2025-11-08 13:11:18 +0100

    vault backup: 2025-11-08 13:11:18

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -16,7 +16,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
 
 PageTitle.css = `
 .page-title {
-  font-size: 1.75rem;
+  font-size: 2.75rem;
   margin: 0;
   font-family: var(--titleFont);
 }
