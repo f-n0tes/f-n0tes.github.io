aaa652ce42432dd4bd1b80333dfaa981936f2ea9
fredxgit <frederik.rix@gmail.com>
2025-11-12 00:53:04 +0100

    Quartz sync: Nov 12, 2025, 12:53 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "de-DE",
-    baseUrl: "fredxgit.github.io/f_notes",
+    baseUrl: "f-notes.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
