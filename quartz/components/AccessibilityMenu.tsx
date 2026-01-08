4f4a966c0adbaf98f0cfc429afd90ddaa644f24f
fredxgit <frederik.rix@gmail.com>
2026-01-08 21:40:28 +0100

    Quartz sync: Jan 8, 2026, 9:40 PM

--- quartz/components/AccessibilityMenu.tsx
+++ quartz/components/AccessibilityMenu.tsx
@@ -30,11 +30,7 @@ const AccessibilityMenu: QuartzComponent = () => {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="1"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            fill="currentColor"
             width="18"
             height="18"
           >
